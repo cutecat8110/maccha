0ca65e2d3776a97a04f51352c051ed67d57d36a1
Hao <cutecat8110@gmail.com>
2024-07-29 09:36:56 +0800

    feat(footer): icon:hover 樣式 & github 連結

--- src/components/page/PageFooter.vue
+++ src/components/page/PageFooter.vue
@@ -1,13 +1,13 @@
 <template>
   <footer class="footer">
-    <div class="container flex flex-col gap-y-10 md:flex-row md:justify-between">
-      <div class="flex flex-col gap-y-10 items-center md:items-start">
+    <div class="container flex flex-col gap-y-10 lg:flex-row lg:justify-between">
+      <div class="flex flex-col gap-y-10 items-center lg:items-start">
         <!-- 連結．首頁 -->
         <router-link to="/">
-          <SvgLogo class="text-[2.5rem] md:text-[4rem]" />
+          <SvgLogo class="text-[2.5rem] lg:text-[4rem]" />
         </router-link>
 
-        <div class="flex flex-col md:flex-row gap-6 w-full md:px-0">
+        <div class="flex flex-col lg:flex-row gap-6 w-full lg:px-0">
           <!-- 按鈕．聯絡我們 -->
           <UiButton state="outline">CONTACT US</UiButton>
           <!-- 連結．線上商店 -->
@@ -20,21 +20,24 @@
         </div>
       </div>
 
-      <div class="flex flex-col items-center gap-y-10 justify-end md:gap-y-10 md:items-end">
+      <div class="flex flex-col items-center gap-y-10 justify-end lg:gap-y-10 lg:items-end">
         <!-- 社群導航 -->
-        <nav class="flex gap-10 text-icon">
-          <a href="https://www.facebook.com/cutecat8110" target="_blank">
+        <nav class="flex gap-6 text-icon">
+          <a href="https://www.facebook.com/cutecat8110" target="_blank" class="icon-link-base">
             <SvgFacebook class="icon-base" />
           </a>
-          <a href="https://instagram.com/cutecat8110" target="_blank">
+          <a href="https://instagram.com/cutecat8110" target="_blank" class="icon-link-base">
             <SvgInstagram class="icon-base" />
           </a>
-          <a href="https://x.com/cutecat8110" target="_blank">
+          <a href="https://x.com/cutecat8110" target="_blank" class="icon-link-base">
             <SvgTwitter class="icon-base" />
+          </a>
+          <a href="https://github.com/cutecat8110/matcha" target="_blank" class="icon-link-base">
+            <SvgGithub class="icon-base" />
           </a>
         </nav>
         <!-- 版權聲明 -->
-        <p class="text-center md:text-right md:leading-10">&copy; MATCHA. All Rights Reserved.</p>
+        <p class="text-center lg:text-right lg:leading-10">&copy; MATCHA. All Rights Reserved.</p>
       </div>
     </div>
   </footer>
