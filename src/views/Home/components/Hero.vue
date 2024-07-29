7512e33a4da1cf8298634d104be455062c3a3b56
Hao <cutecat8110@gmail.com>
2024-07-29 10:33:19 +0800

    feat(hero): 背景圖更新

--- src/views/Home/components/Hero.vue
+++ src/views/Home/components/Hero.vue
@@ -1,13 +1,26 @@
 <template>
-  <section class="hero-wrapper"></section>
+  <section class="hero-wrapper">
+    <div class="mask"></div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
 .hero-wrapper {
-  @apply bg-system-gray-600 min-h-screen;
-  background-image: url('https://dummyimage.com/1920x1080/454545/fff&text=Hero');
+  @apply bg-system-gray-600 h-screen;
+  background-image: url('/img/hero.jpg');
   background-position: center;
-  background-size: contain;
+  background-size: cover;
   background-repeat: no-repeat;
+}
+
+.mask {
+  height: 100%;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(0, 0, 0, 0.4) 30%,
+    rgba(0, 0, 0, 0.4) 70%,
+    rgba(0, 0, 0, 0.6) 100%
+  );
 }
 </style>
