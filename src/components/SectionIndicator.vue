b1c29ea90dd60c694b3304af50b55e8cd4e0f005
Hao <cutecat8110@gmail.com>
2024-07-31 10:26:33 +0800

    chore(story): rwd調整-圖文組合

--- src/components/SectionIndicator.vue
+++ src/components/SectionIndicator.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex items-center gap-x-2 text-sub-title uppercase">
+  <h3 class="flex items-center gap-x-2 text-sub-title uppercase">
     <span class="h-[1px] w-10 bg-gradient-to-r from-white/0 to-white/80"></span>
     {{ props.text }}
-  </div>
+  </h3>
 </template>
 
 <script lang="ts" setup>
--- src/views/Home/components/Story.vue
+++ src/views/Home/components/Story.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="story-wrapper">
+  <section class="story-wrapper space-y-20 lg:space-y-[12.5rem]">
     <div class="container space-y-10 lg:space-y-20">
       <!-- 標題 -->
       <div class="space-y-2 text-center">
@@ -13,30 +13,55 @@
         <SectionIndicator text="concept" />
 
         <!-- 圖文區塊 -->
-        <div class="space-y-20">
+        <div class="space-y-10 lg:space-y-20">
           <!-- 圖文組合 1 -->
-          <div class="flex flex-col items-center gap-10 lg:flex-row-reverse lg:gap-20">
+          <div class="story-content reverse">
             <!-- 圖片 -->
-            <div class="story-img-wrapper">
+            <div class="story-img-wrapper md:order-last">
               <img class="story-img" src="/img/story01.jpg" alt="抹茶粉、竹製刷子和抹茶茶碗" />
             </div>
 
             <!-- 文字描述 -->
-            <div
-              class="lg:text-vrl flex shrink-0 flex-col items-center gap-y-6 text-center lg:flex-1 lg:items-start lg:justify-center lg:gap-y-10 lg:text-nowrap lg:p-10"
-            >
-              <p class="text-h6 lg:text-h4">將抹茶融入你的生活方式</p>
+            <div class="story-text-wrapper md:text-vrl md:text-nowrap">
+              <h4 class="story-sub-title">將抹茶融入你的生活方式</h4>
 
-              <div
-                class="flex flex-col items-center gap-y-2 text-body-2 text-system-gray-50 lg:items-start lg:gap-y-4 lg:text-body"
-              >
-                <p>傳統茶道中的抹茶現在以全新方式呈現，</p>
-                <p>不受傳統約束，將抹茶融入您的生活。</p>
-              </div>
+              <p class="story-text">
+                傳統茶道中的抹茶現在以全新方式呈現， <br />
+                不受傳統約束，將抹茶融入您的生活。
+              </p>
+            </div>
+          </div>
+
+          <!-- 圖文組合 2 -->
+          <div class="story-content">
+            <!-- 圖片 -->
+            <div class="story-img-wrapper">
+              <img class="story-img" src="/img/story02.jpg" alt="一盤抹茶粉、盛滿抹茶粉的湯杓" />
+            </div>
+
+            <!-- 文字描述 -->
+            <div class="story-text-wrapper">
+              <h4 class="story-sub-title">抹茶：天然的能量源</h4>
+
+              <p class="story-text">
+                抹茶以整片茶葉研磨而成，是營養豐富的超級食物。 <br />
+                富含抗氧化成分和天然咖啡因，每一杯都是理想的能量飲品選擇。
+              </p>
             </div>
           </div>
         </div>
       </div>
+    </div>
+
+    <div>
+      <h4>
+        雖然不為人所熟知，<br />
+        但抹茶對身心有著眾多益處。
+      </h4>
+
+      <ul>
+        <li></li>
+      </ul>
     </div>
   </section>
 </template>
@@ -46,11 +71,35 @@
   @apply common-section-padding bg-system-dark text-white;
 }
 
-.story-img-wrapper {
-  @apply shrink-0 px-5 md:px-0 lg:flex-1;
+.story-content {
+  @apply flex flex-col items-center gap-10 md:grid md:grid-cols-2 lg:gap-20;
+  &.reverse {
+    @apply md:flex-row-reverse;
+  }
 }
 
 .story-img {
-  @apply w-full md:max-w-[32.25rem] lg:max-w-max;
+  @apply h-full w-full object-cover;
+}
+
+.story-text-wrapper {
+  @apply flex flex-col items-center gap-y-6 text-center md:items-start md:justify-center md:p-10 md:text-start lg:gap-y-10;
+}
+
+.md\:text-vrl {
+  .story-sub-title {
+    @apply md:tracking-[.2em];
+  }
+  .story-text {
+    @apply md:tracking-[.5em];
+  }
+}
+
+.story-sub-title {
+  @apply text-h6 lg:text-h4 lg:leading-none;
+}
+
+.story-text {
+  @apply text-body-2 leading-6 text-system-gray-50 lg:text-body lg:leading-8;
 }
 </style>
